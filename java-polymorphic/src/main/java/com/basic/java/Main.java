58d97e71ac34446b434dd50349e5dc8733476581
MasterTj <798750509@qq.com>
2018-12-13 09:31:35 +0800

    Java 成员变量的继承.... 2018/12/13 9:31

--- java-polymorphic/src/main/java/com/basic/java/Main.java
+++ java-polymorphic/src/main/java/com/basic/java/Main.java
@@ -2,11 +2,35 @@ package com.basic.java;
 
 /**
  * locate com.basic.java
- * Created by MasterTj on 2018/12/11.
+ * Created by MasterTj on 2018/12/13.
  */
+class Father {
+    int i = 10;
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
+
+class Son extends Father {
+    int i = 20;
+
+    public void show() {
+        System.out.println("super.i : " + super.i + ";this.i : " + this.i);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        People people=new People();
-        people.staticRide(new Bicyle());
+        Son son = new Son();
+        Father parent = new Son();
+
+        System.out.println(parent.i);
+        System.out.println(son.i);
+        System.out.println();
+
+        son.setI(100);
+        son.show();
+        System.out.println(son.i);
     }
 }
