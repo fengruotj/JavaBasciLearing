46953644ae0d0974b1a6f790bcbc6dd7c0296338
MasterTj <798750509@qq.com>
2018-12-17 19:32:20 +0800

    Java数组.... 2018/12/17 19:32

---
+++ java-generics/src/main/java/com/basic/java/ListFun.java
@@ -0,0 +1,19 @@
+package com.basic.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * locate com.basic.java
+ * Created by MasterTj on 2018/12/17.
+ */
+public class ListFun {
+    public static void main(String[] args) {
+        List list=new ArrayList<>();
+        list.add(new Fruit());
+        list.add(new Apple());
+        list.add(new Orange());
+
+        System.out.println(list);
+    }
+}
