552c09670b27901819eef775c725d861cfb84256
MasterTj <798750509@qq.com>
2018-12-21 21:30:37 +0800

    Java 内存模型 volatile关键字使用.... 2018/12/21 21:30

---
+++ java-jvm/src/main/java/com/basic/java/VectorTest.java
@@ -0,0 +1,44 @@
+package com.basic.java;
+
+import java.util.Vector;
+
+/**
+ * locate com.basic.java
+ * Created by MasterTj on 2018/12/21.
+ */
+public class VectorTest {
+    private static Vector<Integer> vector = new Vector<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            for(int i = 0; i < 10; i++){
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                public void run() {
+                    for(int i = 0; i < vector.size(); i++){
+                        vector.remove(i);
+                    }
+                }
+            });
+
+            Thread printThread = new Thread(new Runnable() {
+                public void run() {
+                    for(int i = 0; i < vector.size(); i++){
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        System.out.println(vector.get(i));
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+        }
+    }
+}
